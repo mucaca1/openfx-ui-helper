f2e6415c1225c84d5f7281d0aef93989bcf70ee9
Mucaca <matejkrc1@gmail.com>
2023-12-11 20:35:50 +0100

    Add pane for Paging Bar

---
+++ uifx-helper-core/src/main/java/org/bh/uifxhelpercore/table/PagingBar.java
@@ -0,0 +1,8 @@
+package org.bh.uifxhelpercore.table;
+
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class PagingBar extends Pane {
+    TextField pageValue = new TextField();
+}
