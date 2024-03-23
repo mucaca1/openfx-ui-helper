12070a3ea888a51f00de73c458c18cbe433ed901
Mucaca <matejkrc1@gmail.com>
2024-03-23 13:56:00 +0100

    Add wizard class

--- uifx-helper-core/src/main/java/org/bh/uifxhelpercore/wizard/DynamicWizard.java
+++ uifx-helper-core/src/main/java/org/bh/uifxhelpercore/wizard/DynamicWizard.java
@@ -1,4 +1,21 @@
 package org.bh.uifxhelpercore.wizard;
 
-public class DynamicWizard {
+import javafx.stage.Stage;
+import org.bh.uifxhelpercore.button.ButtonAdvancedBar;
+
+import java.util.List;
+
+public class DynamicWizard extends Stage {
+
+    private ButtonAdvancedBar buttonBar;
+
+    private List<WizardStage> stages;
+
+    private int actualStageIndex;
+
+    public DynamicWizard() {
+        actualStageIndex = 0;
+    }
+
+
 }
---
+++ uifx-helper-core/src/main/java/org/bh/uifxhelpercore/wizard/WizardStage.java
@@ -0,0 +1,9 @@
+package org.bh.uifxhelpercore.wizard;
+
+import javafx.scene.Node;
+
+public class WizardStage {
+
+    private Node form;
+
+}
